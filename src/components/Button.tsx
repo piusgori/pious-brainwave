ab5e79ca094f3d60e32e226b761a9f34bb49fa81
piusgori <piusgori@gmail.com>
2024-07-02 23:07:47 +0300

    Add hambuger

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ const Button = ({ children, className, href, onClick, px, white }: BT) => {
     const spanClasses = 'relative z-10'
 
     const renderButton = (
-        <button className={classes}>
+        <button className={classes} onClick={onClick}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
